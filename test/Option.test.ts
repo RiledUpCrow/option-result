9316b3e9bdec97f62cb99e9651250c3be676a695
Jakub Sapalski <coosheck@gmail.com>
2018-09-24 11:10:05 +0200

    Implement the rest of Option tests

--- test/Option.test.ts
+++ test/Option.test.ts
@@ -57,4 +57,91 @@ describe('Option', () => {
       expect(result).toBe('bar');
     });
   });
+
+  describe('Filtering value', () => {
+    it('passes a correct value', () => {
+      const option = Option.some(4);
+      const result = option.if(val => val % 2 === 0).get();
+      expect(result).toBe(4);
+    });
+
+    it('filters an incorrect value', () => {
+      const option = Option.some(3);
+      const result = option.if(val => val % 2 === 0).isNone();
+      expect(result).toBe(true);
+    });
+
+    it('skips an empty Option', () => {
+      const option = Option.none<number>();
+      const result = option.if(val => val % 2 === 0).isNone();
+      expect(result).toBe(true);
+    });
+  });
+
+  describe('Mapping values', () => {
+    it('maps a value to another', () => {
+      const option = Option.some(10);
+      const result = option.map(val => `Test: ${val * 2}`).get();
+      expect(result).toBe('Test: 20');
+    });
+
+    it("doesn't map when empty", () => {
+      const option = Option.none<number>();
+      const result = option.map(val => `Test: ${val * 2}`).isNone();
+      expect(result).toBe(true);
+    });
+  });
+
+  describe('Mapping and flattening values', () => {
+    it('maps a value to an existing value', () => {
+      const option = Option.some(10);
+      const result = option
+        .flatMap(val => Option.some(`Test: ${val * 2}`))
+        .get();
+      expect(result).toBe('Test: 20');
+    });
+
+    it('maps a value to an an empty option', () => {
+      const option = Option.some(10);
+      const result = option.flatMap(val => Option.none<string>()).isNone();
+      expect(result).toBe(true);
+    });
+
+    it("Doesn't map an empty value", () => {
+      const option = Option.none<number>();
+      const result = option
+        .flatMap(val => Option.some(`Test: ${val * 2}`))
+        .isNone();
+      expect(result).toBe(true);
+    });
+  });
+
+  describe('Converting to an array', () => {
+    it('converts a value to an array', () => {
+      const option = Option.some('foo');
+      const result = option.toArray();
+      expect(result).toHaveLength(1);
+      expect(result[0]).toBe('foo');
+    });
+
+    it('converts an empty option to an empty array', () => {
+      const option = Option.none();
+      const result = option.toArray();
+      expect(result).toHaveLength(0);
+    });
+  });
+
+  describe('Converting to a string', () => {
+    it('converts existing option to a string', () => {
+      const option = Option.some('foo');
+      const result = option.toString();
+      expect(result).toBe('Some(foo)');
+    });
+
+    it('converts empty option to a string', () => {
+      const option = Option.none();
+      const result = option.toString();
+      expect(result).toBe('None()');
+    });
+  });
 });
